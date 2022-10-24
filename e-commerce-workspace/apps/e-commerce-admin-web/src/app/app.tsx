8700afe6c59bd8d4890ba3eedfd343982aae4363
Giorgi Lobjanidze <53515296+lobzhana@users.noreply.github.com>
2022-10-24 19:48:58 +0400

    e-commerce-0002-new-projects

---
+++ e-commerce-workspace/apps/e-commerce-admin-web/src/app/app.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+
+    </StyledApp>
+  );
+}
+
+export default App;
